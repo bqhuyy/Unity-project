d8a6a54730dd99ed40ecd0a10257ee8d93fa25c9
bqhuy <bqhuy@apcs.vn>
2019-06-10 22:54:14 +0700

    [#A03] - Move, jump, and die when collision with monster

---
+++ A03/Assets/Script/BoardManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    public static BoardManager instance;
+    public List<Sprite> characters = new List<Sprite>();
+    public GameObject tile;
+    public int xSize, ySize;
+    private GameObject[,] tiles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = GetComponent<BoardManager>();
+        Vector2 offset = tile.GetComponent<SpriteRenderer>().bounds.size;
+        CreateBoard(offset.x, offset.y);
+    }
+
+    private void CreateBoard( float xOffset, float yOffset)
+    {
+        tiles = new GameObject[xSize, ySize];
+
+        float startX = transform.position.x;
+        float startY = transform.position.y;
+        for (int x=0; x< xSize; ++x)
+        {
+            for(int y =0; y<ySize; ++y)
+            {
+                GameObject newTile = Instantiate(tile, 
+                    new Vector3(startX + (xOffset * x), startY + (yOffset * y), 0), 
+                    tile.transform.rotation);
+                tiles[x, y] = newTile;
+                newTile.transform.parent = transform;
+                Sprite newSprite = characters[Random.Range(0, characters.Count)];
+                newTile.GetComponent<SpriteRenderer>().sprite = newSprite;
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ A03/Assets/Script/Kirby_KinematicControl.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Kirby_KinematicControl : MonoBehaviour
+{
+    private Rigidbody2D myRigidbody;
+    private Animator myAnimator;
+
+    [SerializeField]
+    private float movementSpeed = 1.0f;
+
+    private bool facingRight;
+
+    [SerializeField]
+    private Transform[] groundPoints;
+
+    [SerializeField]
+    private float groundRadius;
+
+    [SerializeField]
+    private LayerMask whatIsGround;
+
+    private bool isGrounded;
+
+    private bool jump;
+
+    [SerializeField]
+    private bool airControl;
+
+    [SerializeField]
+    private float jumpForce;
+
+    const int airLayer = 1;
+
+    private string[] monster = new string[] {
+        "UmbrellaOrange",
+        "ElectricOrange",
+        "Sword"
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        facingRight = true;
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (gameObject)
+        {
+            HandleInput();
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (gameObject)
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            isGrounded = IsGrounded();
+
+            HandleMovement(horizontal);
+            Flip(horizontal);
+            HandleLayers();
+            ResetValues();
+        }
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump = true;
+        }
+    }
+
+    private void ResetValues()
+    {
+        jump = false;
+    }
+
+    private void HandleMovement(float horizontal)
+    {
+        if (myRigidbody.velocity.y < 0)
+        {
+            myAnimator.SetBool("land", true);
+        }
+
+        if (isGrounded && jump)
+        {
+            isGrounded = false;
+            myRigidbody.AddForce(new Vector2(0, jumpForce));
+            myAnimator.SetTrigger("jump");
+        }
+
+        myRigidbody.AddForce(new Vector2(horizontal * movementSpeed, 0));
+        myAnimator.SetFloat("speed", Mathf.Abs(horizontal));
+    }
+
+    private void Flip(float horizontal)
+    {
+        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+            Vector3 theScale = transform.localScale;
+
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        if (myRigidbody.velocity.y <= 0)
+        {
+            foreach (Transform point in groundPoints)
+            {
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(point.position, groundRadius, whatIsGround);
+                
+                for (int i = 0; i < colliders.Length; i++)
+                {
+                    if (colliders[i].gameObject != gameObject)
+                    {
+                        myAnimator.ResetTrigger("jump");
+                        myAnimator.SetBool("land", false);
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private void HandleLayers()
+    {
+        if (!isGrounded)
+        {
+            myAnimator.SetLayerWeight(airLayer, 1);
+        }
+        else
+        {
+            myAnimator.SetLayerWeight(airLayer, 0);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (monster.Contains(collision.gameObject.name))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
